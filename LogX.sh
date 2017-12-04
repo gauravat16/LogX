d7f77a786b96e95c11508b9c6ec9deb9f672a274
gsharm5u <gauravsati19@gmail.com>
2017-12-05 00:43:37 +0530

    Auto delete after n days

--- LogX.sh
+++ LogX.sh
@@ -8,66 +8,6 @@
 # Reset
 Color_Off='\033[0m'       # Text Reset
 
-# Regular Colors
-Black='\033[0;30m'        # Black
-Red='\033[0;31m'          # Red
-Green='\033[0;32m'        # Green
-Yellow='\033[0;33m'       # Yellow
-Blue='\033[0;34m'         # Blue
-Purple='\033[0;35m'       # Purple
-Cyan='\033[0;36m'         # Cyan
-White='\033[0;37m'        # White
-
-# Bold
-BBlack='\033[1;30m'       # Black
-BRed='\033[1;31m'         # Red
-BGreen='\033[1;32m'       # Green
-BYellow='\033[1;33m'      # Yellow
-BBlue='\033[1;34m'        # Blue
-BPurple='\033[1;35m'      # Purple
-BCyan='\033[1;36m'        # Cyan
-BWhite='\033[1;37m'       # White
-
-# Underline
-UBlack='\033[4;30m'       # Black
-URed='\033[4;31m'         # Red
-UGreen='\033[4;32m'       # Green
-UYellow='\033[4;33m'      # Yellow
-UBlue='\033[4;34m'        # Blue
-UPurple='\033[4;35m'      # Purple
-UCyan='\033[4;36m'        # Cyan
-UWhite='\033[4;37m'       # White
-
-# Background
-On_Black='\033[40m'       # Black
-On_Red='\033[41m'         # Red
-On_Green='\033[42m'       # Green
-On_Yellow='\033[43m'      # Yellow
-On_Blue='\033[44m'        # Blue
-On_Purple='\033[45m'      # Purple
-On_Cyan='\033[46m'        # Cyan
-On_White='\033[47m'       # White
-
-# High Intensity
-IBlack='\033[0;90m'       # Black
-IRed='\033[0;91m'         # Red
-IGreen='\033[0;92m'       # Green
-IYellow='\033[0;93m'      # Yellow
-IBlue='\033[0;94m'        # Blue
-IPurple='\033[0;95m'      # Purple
-ICyan='\033[0;96m'        # Cyan
-IWhite='\033[0;97m'       # White
-
-# Bold High Intensity
-BIBlack='\033[1;90m'      # Black
-BIRed='\033[1;91m'        # Red
-BIGreen='\033[1;92m'      # Green
-BIYellow='\033[1;93m'     # Yellow
-BIBlue='\033[1;94m'       # Blue
-BIPurple='\033[1;95m'     # Purple
-BICyan='\033[1;96m'       # Cyan
-BIWhite='\033[1;97m'      # White
-
 # High Intensity backgrounds
 On_IBlack='\033[0;100m'   # Black
 On_IRed='\033[0;101m'     # Red
@@ -91,6 +31,7 @@ options="$@"
 LOG_PROPERTIES=""
 MAX_LOG_FILE_SIZE=""
 COMPRESS_LOGS=""
+DELETE_AFTER="30" #Default - 30 days
 TAG="LOGX-INTERNAL-LOG"
 
 
@@ -147,12 +88,30 @@ load_vars(){
 							"options" )
 								options="$value"
 								;;
+							"DeleteAfter" )
+								DELETE_AFTER="$value"
+			
 
 						esac			
 
 		fi
 	done < "$LOG_PROPERTIES"
 
+}
+
+
+delete_old_logs(){
+	local log_name=$1
+	local older_than=$2
+
+		find *"$log_name"* -mtime "$older_than" | while read line ; do
+			if [[ ! -z "$line" ]]; then
+				rm "$line"
+			fi
+		done
+
+	
+	
 }
 
 compress_logs(){
@@ -167,6 +126,7 @@ write_logs(){
 	local current_dir=$PWD
 	local time_stamp=$(date +"%Y-%m-%d_%H-%M-%S")
 
+
 	if [[ ! -d "$LOG_LOCATION" ]]
 		then
 			mkdir $LOG_LOCATION
@@ -175,7 +135,12 @@ write_logs(){
 	fi	
 	local log_name=$1
 	local log_line=$2
+	#Delete old logs
+	delete_old_logs "$log_name" "$DELETE_AFTER"
+
 	local latest_file=$(ls -t | grep "$log_name"|head -n1 )
+
+	
 	if [[ -z "$latest_file" ]]
 		then
 		echo $log_line >> "$log_name"_"$time_stamp".log
